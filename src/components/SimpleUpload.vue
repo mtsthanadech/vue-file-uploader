9e5f5ba026729ed7576a13d95030d9f0e763cb06
lionmark02 <withusiri.12594@mail.kmutt.ac.th>
2018-11-28 18:51:09 +0700

    Update SimpleUpload.vue

--- src/components/SimpleUpload.vue
+++ src/components/SimpleUpload.vue
@@ -61,7 +61,7 @@ export default {
                 this.message = tooLarge ? `Too large, Max size is ${MAX_SIZE/1000}kb` : "Only JSON file are allowed";
             }
         },
-        sendFile() {
+        async sendFile() {
             const formData = new FormData();
             const url = "https://35.198.215.67:3344/upload";
             const urlnew = "https://35.198.215.67/seniorproject/json/" + this.dbname;
@@ -71,7 +71,7 @@ export default {
             // formData.append('dbnam',this.dbname+".json");
 
             try {
-                axios.post(url, formData);
+                await axios.post(url, formData);
                 this.message = "File has been uploaded";
                 this.file = "";
                 this.error = false;
