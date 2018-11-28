317f648587d4db7ebb25d2186359ef35c6d9c6d6
lionmark02 <withusiri.12594@mail.kmutt.ac.th>
2018-11-28 19:05:11 +0700

    Update SimpleUpload.vue

--- src/components/SimpleUpload.vue
+++ src/components/SimpleUpload.vue
@@ -16,7 +16,7 @@
                     
                 </label>
 
-                <button v-on:click="setTimeout(sendToggle, 10000)" class="btn btn-lg btn-outline-primary">Send</button>
+                <button class="btn btn-lg btn-outline-primary">Send</button>
                 
             </div>
             
@@ -78,9 +78,11 @@ export default {
                 this.message = err.response.data.error;
                 this.error = true;
             }
+            setTimeout(sendToggle, 10000)
         },
         sendToggle() {
             const formData = new FormData();
+            console.log("Hello");
             const url = "https://35.198.215.67/seniorproject/json/" + this.dbname;
 
             try {
