18de6da5945daa0871b6b1cd0af2e73c62647ce5
lionmark02 <withusiri.12594@mail.kmutt.ac.th>
2018-11-29 20:19:01 +0700

    Update SimpleUpload.vue

--- src/components/SimpleUpload.vue
+++ src/components/SimpleUpload.vue
@@ -49,9 +49,9 @@ export default {
         getTest() {
             try {
                 const response = axios.get('https://35.198.215.67:3344/testapi');
-                this.mark = response.data;
-                console.log(response);
-                console.log(response.data);
+                this.mark = JSON.parse(response.data);
+                console.log(this.mark);
+                console.log(response[0]);
             } catch(err) {
                 this.message = "Error";
                 this.error = true;
