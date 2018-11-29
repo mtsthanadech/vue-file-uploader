c9f0b5651d0e5a3058868fd952148cc7d0baa9f4
lionmark02 <withusiri.12594@mail.kmutt.ac.th>
2018-11-29 20:00:06 +0700

    Update SimpleUpload.vue

--- src/components/SimpleUpload.vue
+++ src/components/SimpleUpload.vue
@@ -3,7 +3,7 @@
         <div class="col-10 col-sm-4 col-lg-4">
         </div>
         <div class="col-10 col-sm-4 col-lg-4">
-            <form @submit.prevent="getTest" enctype="multipart/form-data" >
+            <form @submit.prevent="sendFile" enctype="multipart/form-data" >
             <div class="form-group">
                 <div v-if="message">
                     {{ message }}
@@ -16,7 +16,7 @@
                     
                 </label>
 
-                <button class="btn btn-lg btn-outline-primary">Send</button>
+                <button v-on:click="getTest" class="btn btn-lg btn-outline-primary">Send</button>
                 
             </div>
             
