c03472ca058d0369121d7931ef3031d31ea0d4ca
Thanadech Srisukawasu <mts.thanadech@gmail.com>
2018-12-01 21:23:54 +0700

    add 3

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,10 @@
         <match-column />
       </div>
       
+      <div>
+        <input-and-review />
+      </div>
+      
     </div>
   <!-- </div> -->
 
@@ -19,12 +23,14 @@
 <script>
 import SimpleUpload from './components/SimpleUpload.vue'
 import MatchColumn from './components/MatchColumn.vue'
+import InputAndReview from './components/InputAndReview.vue'
 
 export default {
   name: 'app',
   components: { 
     'simple-upload': SimpleUpload,
-    'match-column': MatchColumn
+    'match-column': MatchColumn,
+    'input-and-review': InputAndReview
   }
 }
 </script>
---
+++ src/components/InputAndReview.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="row">
+        <div class="col-10 col-sm-4 col-lg-4">
+        </div>
+        <div class="col-10 col-sm-4 col-lg-4">
+
+            <b-card title="Input text in Thai">
+                <b-input type="text" v-model="payload" @change="onChange"/>
+                <b-button @click="sendMessage" variant="primary">Preview</b-button>
+                <p>eiei : {{ message }} - {{ payload }}</p>
+
+                
+            </b-card>
+        </div>
+        
+    </div>
+</template>
+<script>
+import axios from 'axios'; 
+export default {
+
+    name: "InputAndReview",
+    data() {
+        return {
+            payload: "",
+            message: ""
+        }
+    },
+    methods: {
+        onChange: function() {
+
+            this.message = this.payload;
+        },
+        sendMessage(){
+            const message = this.payload;
+        }
+    }
+}
+</script>
