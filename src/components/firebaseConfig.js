292cdcd9cf060d7af158b5b1cb8e0422384d42b9
Mt's <mts.thanadech@gmail.com>
2018-12-03 18:14:53 +0700

    Update firebaseConfig.js

--- src/components/firebaseConfig.js
+++ src/components/firebaseConfig.js
@@ -1,8 +1,3 @@
 export default {
-    apiKey: "AIzaSyDl2Dexy5m9mONTfNC3KwlLMfyZ7AyQZ34",
-    authDomain: "senior-project-cpe-kmutt.firebaseapp.com",
-    databaseURL: "https://senior-project-cpe-kmutt.firebaseio.com",
-    projectId: "senior-project-cpe-kmutt",
-    storageBucket: "senior-project-cpe-kmutt.appspot.com",
-    messagingSenderId: "153768348493"
+    //copy key from https://console.firebase.google.com/u/0/project/senior-project-cpe-kmutt/authentication/
 };
