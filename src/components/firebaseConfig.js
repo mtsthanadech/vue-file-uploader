3362f165712b694c149ab5918c81eb8ce86080e5
Mt's <mts.thanadech@gmail.com>
2019-09-29 00:06:05 +0700

    hidden firebase key

--- src/components/firebaseConfig.js
+++ src/components/firebaseConfig.js
@@ -1,8 +1,8 @@
 export default {
-    apiKey: "AIzaSyDl2Dexy5m9mONTfNC3KwlLMfyZ7AyQZ34",
-    authDomain: "senior-project-cpe-kmutt.firebaseapp.com",
-    databaseURL: "https://senior-project-cpe-kmutt.firebaseio.com",
-    projectId: "senior-project-cpe-kmutt",
-    storageBucket: "senior-project-cpe-kmutt.appspot.com",
-    messagingSenderId: "153768348493"
+    apiKey: "API-KEY",
+    authDomain: "PROJECT-NAME.firebaseapp.com",
+    databaseURL: "https://PROJECT-NAME.firebaseio.com",
+    projectId: "PROJECT-NAME",
+    storageBucket: "PROJECT-NAME.appspot.com",
+    messagingSenderId: ""
 };
